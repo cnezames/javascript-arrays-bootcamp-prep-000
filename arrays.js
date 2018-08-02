b0a29019037f69880cc89409074b545399bef2c8
Cynthia Nezames <cnezames@gmail.com>
2018-08-02 17:52:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.shift[element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
